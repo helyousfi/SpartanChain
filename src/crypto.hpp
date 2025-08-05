9608431ddb6f663f6113686708b6521635288163
Hamza EL Yousfi <visioncomputer200@gmail.com>
2025-08-05 11:13:13 +0200

    Update crypto.hpp

--- src/crypto.hpp
+++ src/crypto.hpp
@@ -1,5 +1,5 @@
 namespace Crypto {
-	std::string sha256(cont std::string& input);
+	std::string sha256(const  std::string& input);
 	EVP_PKEY* generateKey();
 	std::string getPublicKeyString(EVP_PKEY* key);
 	std::string sign(const std::string& message, EVP_PKEY* key);
