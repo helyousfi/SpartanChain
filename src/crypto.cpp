1436e03cd99f6ed608fb4c36d4df89932d18f3c8
Hamza EL Yousfi <visioncomputer200@gmail.com>
2025-08-05 13:57:41 +0200

    Update crypto.cpp

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -11,9 +11,9 @@
 
 namespace Crypto {
 
-    std::string sha256(const std::string& input) {
+    std::string sha256(const std::string& input) { // Secure Hash Algorithm 256-bit.
         unsigned char hash[SHA256_DIGEST_LENGTH];
-        SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
+        SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash); 
         std::ostringstream result;
         for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
             result << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
@@ -110,3 +110,4 @@ namespace Crypto {
     }
 
 }
+
