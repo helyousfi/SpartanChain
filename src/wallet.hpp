f430630cb023afcfe66ea2e3d01848c978f06c68
Hamza EL Yousfi <visioncomputer200@gmail.com>
2025-08-05 14:13:23 +0200

    Update wallet.hpp

--- src/wallet.hpp
+++ src/wallet.hpp
@@ -4,8 +4,17 @@ class Wallet{
 		std::string address;
 
 		Wallet();
+		~Wallet();
+
 		void save(const std::string& path);
 	       	static Wallet load(const std::string& path);
+		
 		std::string getAddress() const;
-		Transaction createTransaction(const std::string& to, int amount); 	
+		Transaction createTransaction(const std::string& to, int amount); 
+		std::string getPublicKeyString() const;
+
+		Wallet(const Wallet&) = delete;
+		Wallet& operator=(const Wallet&) = delete;
+		Wallet(Wallet&& other) noexcept;
+		Wallet& operator=(Wallet&& other) noexcept;
 };
