07b49eb4e487f94406bfc84981a51b9227a624f2
Hamza EL Yousfi <visioncomputer200@gmail.com>
2025-08-05 10:54:05 +0200

    Update transaction.hpp

--- src/transaction.hpp
+++ src/transaction.hpp
@@ -1,10 +1,10 @@
 class Transaction{
-	public:
+	private:
 		std::string from;
 		std::string to;
 		double amount;
 		std::string signature;
-
+	public:
 		Transaction(const std::string& fromAdr, const std::string& toAdr, double amnt);
 		void signTransaction(EVP_PKEY* privateKey);
 		bool isValid() const;
